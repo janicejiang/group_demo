7da73133af69a29c98ad88a361fd7a3412bf7a43
janice <janicejiang.d@gmail.com>
2017-09-01 21:14:51 +0800

    add post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :groups # 1对多 has_many 关联, 另一个模型使用复数形式 
+  has_many :groups # 1对多 has_many 关联, 另一个模型使用复数形式
+  has_many :posts
 end
