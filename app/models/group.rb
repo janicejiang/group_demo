12116a95cebc2210a98d56f11aff709fa2778ac6
janice <janicejiang.d@gmail.com>
2017-09-02 21:36:49 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ApplicationRecord
 
   belongs_to :user # 1对1 belongs_to 关联声明中必须使用单数形式
   has_many :posts
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :groups # 1对多 has_many 关联, 另一个模型使用复数形式
   has_many :posts
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships,
+                                 :source => :group
+
+  def is_member_of?(group)
+    # participated_groups 返回一个数组包含当前调用 is_member_of(group) 方法的用户参加的所有小组实例
+    # 对应的 SQLite SQL 语句如下:
+    # SELECT "groups".* FROM "groups" INNER JOIN "group_relationships"
+    # ON "groups"."id" = "group_relationships"."group_id"
+    # WHERE "group_relationships"."user_id" = ?
+
+    participated_groups.include?(group)
+    # 调用 Array#include?, 如果 group 变量包含在 participated_groups 数组中, 则返回 true
+  end
 end
