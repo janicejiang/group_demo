4ad0f9ed7bffc5b68575e67b38dbd4814fb0d2f2
janice <janicejiang.d@gmail.com>
2017-09-03 09:19:49 +0800

    def join/quit group in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,21 @@ class User < ApplicationRecord
   has_many :participated_groups, :through => :group_relationships,
                                  :source => :group
 
+  # participated_groups返回一个数组包含当前用户参加的所有小组实例
+  # 对应的 SQLite SQL 语句如下:
+  # SELECT "groups".* FROM "groups" INNER JOIN "group_relationships"
+  # ON "groups"."id" = "group_relationships"."group_id"
+  # WHERE "group_relationships"."user_id" = ?
   def is_member_of?(group)
-    # participated_groups 返回一个数组包含当前调用 is_member_of(group) 方法的用户参加的所有小组实例
-    # 对应的 SQLite SQL 语句如下:
-    # SELECT "groups".* FROM "groups" INNER JOIN "group_relationships"
-    # ON "groups"."id" = "group_relationships"."group_id"
-    # WHERE "group_relationships"."user_id" = ?
-
     participated_groups.include?(group)
-    # 调用 Array#include?, 如果 group 变量包含在 participated_groups 数组中, 则返回 true
+  end
+  # 调用 Array#include?, 如果传进来的Group实例包含在 participated_groups 数组中, 则返回 true
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
   end
 end
